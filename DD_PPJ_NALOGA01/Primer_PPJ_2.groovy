506b30857214d5aebc1bb5d623e4f8ed5d5b22f9
N1ckProgramming <161765353+NikolaPopovski1@users.noreply.github.com>
2024-05-25 01:37:16 +0200

    Follow progres
    Follow nedokončan

---
+++ DD_PPJ_NALOGA01/Primer_PPJ_2.groovy
@@ -0,0 +1,88 @@
+const num spremenljivka = 14 
+const num spremenljivka = 14.421 
+string spremenljivka2 = "restavracija"
+coord coordinate = (46.245267, 15.234526) //(num, num)
+List<tip> name = []
+
+coordinate = (16.245267, 45.234526)
+
+city ime {
+    restaurant r1 {
+        name: "name"
+        shape: { //preverja se, da je oblika smiselna (da se ne križa...)
+            line[(0, 0), (0, 2)] //line[coord, coord]
+            line[(0, 2), (2, 2)]
+            line[(2, 2), (2, 0)]
+            line[(2, 0), (0, 0)]
+        }
+        marker: point[(1, 2)]//Če ni podan se izručna na sredini polygona point[coord]
+        routes: [
+            road dostava1 {
+                name: "Dostava do nekam, nea vem kam"
+                shape : {
+                    line[c1, (4, 2)]
+                    bend[(20, 1), (0, 22), 30]
+                    line[(2, 0), (30, 2)]
+                    bend[line[(0, 5), c4], 0]
+                    bend[line[(20, 24), (0, 2)], 13]
+                }
+            }, 
+            road name {
+                name: "Dostava do nekam, nea vem kam2"
+                bend[line[(30, 0), (42, 2)], 0]
+                bend[line[(0, 1), (30, 2)], 30]
+                line[(0, 5), c1]
+                bend[line[(0, 4), (0, 2)], variable1]
+            }, ...
+        ] //Ce ni podan, je prazen seznam
+    } 
+    
+
+    restaurant r2 {
+        name: "name"
+        shape: {
+            line[(0, 0), (0, 2)]
+            line[(0, 2), (2, 2)]
+            line[(2, 2), (0, 0)]
+        }
+        //tu ni marker zato ga more samo zračunat glede na sredino shape-a
+        //tudi routes ni nastavljen, torej prazen list
+    } 
+    restaurant r3 {
+        name: "name"
+        shape: {
+            line[(0, 0), (0, 2)]
+            line[(0, 2), (2, 2)]
+            line[(2, 2), (2, 0)]
+            line[(2, 0), (2, 4)]
+            line[(2, 4), (0, 0)]
+        }
+        marker: point[(3, 5)]
+    } 
+
+    number p1 = 46.002300
+
+    restaurant r4 {
+        name: "Restavracija zlata srna"
+        shape: {
+            line[(46,928398, 0), (0, 2)]
+            line[(p1, 2), (2, 2)]
+            line[(2, 2), (2, 0)]
+            line[(2, 0), (2 , 4)]
+            line[(2, 4), (0 , 0)]
+        }
+        marker[(x, y)] //središče restavracije (če ni podano se izračuna sredina)
+    } 
+
+    List<tip> name = [random]
+    List<coordinate> = [coord1, coord2...]
+    List<restaurant> = [r1, r2...]
+
+    foreach route in name.routes {
+        print(route.name)
+    }
+
+    foreach x in radius[(1, 4), 4] {
+        highlight(x)
+    }
+}
