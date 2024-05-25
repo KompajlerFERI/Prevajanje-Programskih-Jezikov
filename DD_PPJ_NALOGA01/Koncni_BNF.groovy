878803c2f9af76686d58f9989c2afbb8e5b28f0b
N1ckProgramming <161765353+NikolaPopovski1@users.noreply.github.com>
2024-05-26 01:02:26 +0200

    [DD-167] Dokončan BNF, ki je LL(1)
    LL(1) se nahaja v folderju Naloga_3
    V folderju Naloga_1 je tudi ročno izračunan first in follow, ki je bil proti koncu neuporaben, saj sem ugotovil, da obstaja spletni vir za računanje FIRST-ov in FOLLOW-ov za preverjanje LL(1) BNF-a (toliko, da se vidi odvečno delo in trpljenje ter to da se vidi, da sem delal)

---
+++ DD_PPJ_NALOGA01/Koncni_BNF.groovy
@@ -0,0 +1,121 @@
+Main_Program ::= Main_Program’’ semicol Main_Program’
+Main_Program’ ::= Main_Program semicol Main_Program’
+Main_Program’ ::= ''
+Main_Program’’ ::= Declaration
+Main_Program’’ ::= Assignment
+Main_Program’’ ::= City
+Main_Program’’ ::= For
+Declaration ::= const Declaration’
+Declaration ::= Declaration’
+Declaration’ ::= Type variable equals Expression
+Assignment ::= hash variable equals Expression
+Type ::= type_number
+Type ::= type_string
+Type ::= type_coordinate
+Type ::= type_list lthen Type mthen
+Expression ::= Additive
+Additive ::= Multiplicative Additive’
+Additive’ ::= plus Multiplicative Additive’
+Additive’ ::= minus Multiplicative Additive’
+Additive’ ::= ''
+Multiplicative ::= Exponential Multiplicative’
+Multiplicative’ ::= times Exponential Multiplicative’
+Multiplicative’ ::= divide Exponential Multiplicative’
+Multiplicative’ ::= integer-divide Exponential Multiplicative’
+Multiplicative’ ::= ''
+Exponential ::= Unary Exponential’
+Exponential’ ::= pow Unary Exponential’
+Exponential’ ::= ''
+Unary ::= plus Primary
+Unary ::= minus Primary
+Unary ::= Primary
+Primary ::= real
+Primary ::= variable
+Primary ::= lbracket Additive rbracket
+Primary ::= lsq_bracket Inner_List rsq_bracket
+Inner_List ::= Expression Inner_List’
+Inner_List’ ::= comma Expression Inner_List’
+Inner_List’ ::= ''
+City ::= city variable block_start City’ block_end semicol
+City’ ::= City’’’ semicol City’’
+City’’ ::= City’ semicol City’’
+City’’ ::= ''
+City’’’ ::= City_Constructs
+City’’’ ::= Declaration
+City’’’ ::= Assignment
+City’’’ ::= Print
+Print ::= print lbracket Expression rbracket
+City_Constructs ::= Restaurant
+City_Constructs ::= Road
+Restaurant ::= Name Shape Marker Routes
+Name ::= name colon String
+String ::= variable
+String ::= string
+Shape ::= shape colon block_start Lines block_end
+Lines ::= hash Line hash Line hash Line Line’
+Line’ ::= hash Line Line’
+Line’ ::= ''
+Line ::= line lsq_bracket Coord comma Coord rsq_bracket
+Coord ::= variable
+Coord ::= lbracket Expression comma Expression rbracket
+Marker ::= Marker’
+Marker ::= ''
+Marker’ ::= marker colon Point
+Point ::= point lsq_bracket Coord rsq_bracket
+Routes ::= Routes’
+Routes ::= ''
+Routes’ ::= routes colon lsq_bracket Roads rsq_bracket
+Roads ::= Road Road’
+Road’ ::= comma Road Roads’
+Road’ ::= ''
+Road ::= road variable block_start Name Road_Shapes block_end
+Road_Shapes ::= shape colon block_start  Road_Shapes’ Road_Shapes block_end
+Road_Shapes’ ::= Road_Shape semicol Road_Shapes’
+Road_Shapes’ ::= ''
+Road_Shape ::= Line
+Road_Shape ::= Bend
+Bend ::= hash bend lsq_bracket Coord comma Coord comma Expression rsq_bracket
+For ::= foreach variable in Variable block_start Program block_end
+Variable ::= variable
+Variable ::= Radius
+Radius ::= lsq_bracket Coord comma Expression rsq_bracket
+Program ::= Program’’ semicol Program’
+Program’ ::= Program semicol Program’
+Program’ ::= ''
+Program’’ ::= Declaration
+Program’’ ::= Assignment
+Program’’ ::= Print
+Program’’ ::= Highlight
+Highlight ::= highlight lbracket variable rbracket 
+
+const -> const
+variable -> {A,…,Z,a,…,z}{A,…,Z,a,…,z,0,…,9}*
+equals -> =
+type_number -> num
+type_string -> string
+type_coordinate -> coord
+type_list -> List
+lthen -> <
+mthen -> >
+city -> city
+block_start -> {
+block_end -> }
+name -> name
+colon -> :
+string -> “{A,…,Z,a,…,z,0,…,9}*” oz. ASCII
+shape -> shape
+line -> line
+lsq_bracket -> [
+comma -> ,
+rsq_bracket -> ]
+lbracket -> (
+rbracket -> )
+marker  -> marker
+point-> point
+routes-> routes
+road-> road
+bend-> bend
+foreach-> foreach
+highlight -> highlight
+semicol -> ;
+hash -> #
